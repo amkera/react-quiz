9bcd2733fe30e05a799a34fdfdb23ac4bab4829a
amkera <amber_kerawala@berkeley.edu>
2021-10-12 14:49:00 -0400

    Build fetch functionality and add elements to the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions } from "./API";
+import { Difficulty } from "./API";
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [Questions, setQuestions] = useState([]);
+  const [number, useNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -14,10 +26,17 @@ const App = () => {
       <button className="start" onClick={startTrivia}>
         Start Quiz
       </button>
-      <p className="score">Score:</p>
-      <p> Loading Questions...</p>
+      <p className="score">Score: </p>
+      <p>Loading Questions...</p>
 
-      <QuestionCard />
+      {/* <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ?? userAnswers[number]}
+        callback={checkAnswer}
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
-const QuestionCard = () => {
-  return <div>Question Card</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+/**
+ * answers in Props is going to be an array of strings
+ * QuestionCard is a functional component (a type from React itself)
+ * Props is an argument of this functional component
+ */
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div>
+        {answers.map((answer) => {
+          <div>
+            <button disabled={userAnswer} onClick={callback}></button>
+            <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionCard;
